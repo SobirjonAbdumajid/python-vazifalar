d3d371aff63cd62e00bda5dfcc6ef4e98873b500
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-02 11:23:48 +0500

    task

--- 05-ustoz-darslari/lesson9_list_comprahension.py
+++ 05-ustoz-darslari/lesson9_list_comprahension.py
@@ -1,13 +1,82 @@
-_list = [x for x in range(1,5)]
-print(_list)
+# # 1
+# _list = [x for x in range(1,5)]
+# print(_list)
 
-_set = {y for y in range(1,5)}
-print(_set)
+# # 2
+# _set = {y for y in range(1,5)}
+# print(_set)
 
-names = ["Alex", "Ivan", "Sergey"]
-_dict = {x:y for x, y in enumerate(names)}
-print(_dict)
+# # 3
+# names = ["Alex", "Ivan", "Sergey"]
+# _dict = {x:y for x, y in enumerate(names)}
+# print(_dict)
+
+# # 4
+# _tuple = (y for y in range(1,5)) # bu generator bo'ladi
+# print(_tuple)
+
+# # 5
+# def gen_list():
+#     for i in range(5):
+#         yield i
+# l = gen_list()
+# for x in l:
+#     print(x ** 2)
+
+# # 5
+# while names:
+#     for i, v in enumerate(names):
+#         x = names.pop(i)
+# print(names)
+
+# # 6
+# def gen_of(x):
+#     for i in range(x):
+#         yield i
+# n = gen_of(10)
+# for i in n:
+#     print(i)
 
 
-_tuple = (y for y in range(1,5))
-print(_tuple)
+# # 7
+# def find_prime_of(x=7):
+#     _list = []
+#     tub = 1
+#     for i in range(2, x+1):
+#         for j in range(1, i+1):
+#             if i % j == 0:
+#                 tub += 1
+#                 _list.append(i)
+            
+#         if len(_list) == 2:
+#             yield i
+#         _list = []
+
+# n = find_prime_of(12)
+# for i in n:
+#     print(i)
+
+
+# # 8
+# def find_divisin(x=7):
+#     for i in range(1, x+1):
+#         if (i % 4 == 0 and i % 6 != 0) or (i % 4 != 0 and i % 6 == 0):
+#             yield i
+            
+# n = find_divisin(100)
+# for i in n:
+#     print(i)
+
+
+# 9
+def task():
+    for i in range(12):
+        if i % 4 == 0 and i % 6 == 0:
+            continue
+        elif i % 4 == 0 :
+            yield i
+        elif i % 6 == 0 :
+            yield i
+
+for x in task():
+    print(x)
