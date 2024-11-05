85d9a7edfff7e93732a9d6d9f4056b7f9dccce14
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-05 11:22:55 +0500

    tasks

--- 05-ustoz-darslari/lesson10_oop.py
+++ 05-ustoz-darslari/lesson10_oop.py
@@ -37,27 +37,52 @@
     
 
 
-# 3
-class Cats:
+# # 3
+# class Cats:
     
-    def set_name(self, name, color, age):
-        """
-        Example class function
-        """
+#     def set_name(self, name, color, age):
+#         """
+#         Example class function
+#         """
         
+#         self.name = name
+#         self.color = color
+#         self.age = age
+    
+#     def get_name(self):
+#         return self.name, self.color, self.age
+    
+
+# cat1 = Cats() # instance
+# cat2 = Cats()
+
+# cat1.set_name('Oppoqoy', 'black', 1)
+
+# setattr(cat2, 'name', 'Oppoqoy')
+# setattr(cat2, 'color', 'white')
+# setattr(cat2, 'age', 12)
+# # print(getattr(cat2, 'name'))
+# print(cat2.get_name())
+
+# delattr(cat2, 'color')
+# print(cat2.__dict__)
+
+
+class Dogs:
+    def set_attribute(self, name, age, breed, weight=False):
         self.name = name
-        self.color = color
         self.age = age
+        self.weight = weight
+        self.breed = breed
+        
+    def get_attribute(self):
+        return self.name, self.age, self.breed, self.weight
     
-    def get_name(self):
-        return self.name, self.color, self.age
+    def bark(self):
+        if self.weight:
+            return 'This is ' + self.breed
     
+dog1 = Dogs()
 
-cat1 = Cats() # instance
-cat2 = Cats()
-
-cat1.set_name('Oppoqoy', 'black', 1)
-
-setattr(cat2, 'color', 'white')
-setattr(cat2, 'age', 12)
-print(getattr(cat2, 'name'))
+dog1.set_attribute('John', 12, 'Apcharka', 321)
+print(dog1.bark())
