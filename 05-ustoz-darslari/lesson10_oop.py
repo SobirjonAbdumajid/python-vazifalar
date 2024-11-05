c7d2df5c015c8238d4088eb416e4bdef745c2d13
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-05 10:14:02 +0500

    tasks

--- 05-ustoz-darslari/lesson10_oop.py
+++ 05-ustoz-darslari/lesson10_oop.py
@@ -1,25 +1,37 @@
+# 1
+# class Cats:
+#     name = 'Pishak'
+#     color = 'Qora'
+#     age = 2
+    
+#     def set_name(self, name):
+#         """
+#         Example class function
+#         """
+#         self.name = name
+#         print(f"This is {self.name}")
+    
+    
+# cat1 = Cats() # instance
+# cat2 = Cats()
+
+# cat1.name = 'Alex'
+# cat2.name = 'Mushuk'
+
+# cat1.set_name('Oppoqoy')
+# # print(cat1.name)
+# # print(cat2.name)
+# # print(cat1.name)
+# # print(Cats().name)
+
+
 class Cats:
-    name = 'Pishak'
-    color = 'Qora'
-    age = 2
+    name = "Pishak"
     
-    def set_name(self):
-        """
-        Example class function
-        """
-        print("Call Methods form Cats object")
+    def get_pishak_name(self):
+        return 'Meow ' + self.name
     
+cat1 = Cats()
+print(cat1.get_pishak_name())
     
-cat1 = Cats() # instance
-cat2 = Cats()
-
-cat1.name = 'Alex'
-cat2.name = 'Mushuk'
-
-# print(cat2.name)
-# print(cat1.name)
-# print(Cats().name)
-
-
-
 
