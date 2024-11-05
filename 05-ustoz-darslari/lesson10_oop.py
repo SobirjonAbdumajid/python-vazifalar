56fab5433f1f003a9eb118c9aa568331d779ded2
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-05 10:00:55 +0500

    tasks

---
+++ 05-ustoz-darslari/lesson10_oop.py
@@ -0,0 +1,25 @@
+class Cats:
+    name = 'Pishak'
+    color = 'Qora'
+    age = 2
+    
+    def set_name(self):
+        """
+        Example class function
+        """
+        print("Call Methods form Cats object")
+    
+    
+cat1 = Cats() # instance
+cat2 = Cats()
+
+cat1.name = 'Alex'
+cat2.name = 'Mushuk'
+
+# print(cat2.name)
+# print(cat1.name)
+# print(Cats().name)
+
+
+
+
