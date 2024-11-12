3df4ebe548a3ee79b1e0343b29206951ebf661c6
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-12 09:34:48 +0500

    tasks

--- 05-ustoz-darslari/lesson14_magic_methods.py
+++ 05-ustoz-darslari/lesson14_magic_methods.py
@@ -1,10 +1,25 @@
+# # 1
+# class Points:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+    
+#     def __call__(self, *args, **kwds):
+#         return self.x, self.y
+    
+# pt = Points(1,2)
+# print(pt())
+
+
+# 2
 class Points:
     def __init__(self, x, y):
         self.x = x
         self.y = y
     
-    def __call__(self, *args, **kwds):
-        return self.x, self.y
-    
+    @staticmethod
+    def area(r):
+        return 3.14 * r * r
+
 pt = Points(1,2)
-print(pt())
+print(pt.area(3))
