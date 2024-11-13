9ce1e555d076b2153ed242d99ad35611a061b680
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-13 09:40:42 +0500

    tasks

--- 05-ustoz-darslari/lesson15_repr_and_str_dunders.py
+++ 05-ustoz-darslari/lesson15_repr_and_str_dunders.py
@@ -1,13 +1,33 @@
-class Cat:
-    def __init__(self, name):
-        self.name = name
+# # 1
+# class Cat:
+#     def __init__(self, name):
+#         self.name = name
         
-    def __repr__(self):
-        return f'{self.__class__}: {self.name}'
+#     def __repr__(self):
+#         return f'{self.__class__}: {self.name}'
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
     
-ct = Cat("Alex")
-print(ct)
-print(str(ct))
+# ct = Cat("Alex")
+# ct
+# print(str(ct))
+# print(ct)
+
+
+# # 2
+# def example(*args):
+#     print(type(args))
+
+# example([1, 2, 3])
+
+
+# 3
+class Point:
+    def __init__(self, *args):
+        self.__coords = args
+    
+    def __len__(self):
+        return len(self.__coords)
+pt = Point(1,2,3,4,5,6)
+print(len(pt))
