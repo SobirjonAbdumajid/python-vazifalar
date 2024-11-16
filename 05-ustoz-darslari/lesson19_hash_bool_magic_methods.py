a26dcbc30034e5bf23c7a806dedb499b71366c0f
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-16 09:34:21 +0500

    tasks

--- 05-ustoz-darslari/lesson19_hash_bool_magic_methods.py
+++ 05-ustoz-darslari/lesson19_hash_bool_magic_methods.py
@@ -1,14 +1,36 @@
-class Point:
-    def __init__(self, x):
-        self.x = x
+# # 1
+# class Point:
+#     def __init__(self, x):
+#         self.x = x
 
-    # def __eq__(self, value):
-    #     return self.x == value.x and self.y == value.y
+#     # def __eq__(self, value):
+#     #     return self.x == value.x and self.y == value.y
     
-    def __hash__(self):
-        return hash((self.x))
+#     def __hash__(self):
+#         return hash((self.x))
 
-pt1 = Point("dsadsadsa")
+# pt1 = Point("dsadsadsa")
 
-print(hash(pt1))
-print(hash("qeqwewqewq"))
+# print(hash(pt1))
+# print(hash("qeqwewqewq"))
+
+# print(None == None)
+# print(None == None)
+
+
+# 2
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def __bool__(self):
+        print("__bool__")
+        return self.x == self.y
+    
+pt = Point(0, 1)
+
+if pt:
+    print('object pt give true')
+else:
+    print('false')
