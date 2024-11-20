b40536fa16affc1c470d8eefa4af65c70a71cfef
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-20 09:54:31 +0500

    tasks

--- 05-ustoz-darslari/lesson22_multiple_ingeritence.py
+++ 05-ustoz-darslari/lesson22_multiple_ingeritence.py
@@ -1,22 +1,49 @@
+# # 1
+# class Point:
+#     @staticmethod
+#     def draw():
+#         print("draw works")
+
+# class Logger:
+#     @staticmethod
+#     def save_log():
+#         print("Logger works")
+
+# class InsertDatabase:
+#     @staticmethod
+#     def insert():
+#         print("insert works")
+
+# class Notebook(Point, Logger, InsertDatabase):
+#     pass
+
+# n = Notebook()
+# n.draw()
+# n.save_log()
+# n.insert()
+
+
+# 2
 class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
     @staticmethod
     def draw():
         print("draw works")
 
 class Logger:
+    def __init__(self):
+        super.__init__()
     @staticmethod
     def save_log():
         print("Logger works")
 
-class InsertDatabase:
-    @staticmethod
-    def insert():
-        print("insert works")
 
-class Notebook(Point, Logger, InsertDatabase):
+class Notebook(Point, Logger):
     pass
 
-n = Notebook()
+n = Notebook(1,2)
 n.draw()
 n.save_log()
-n.insert()
