93b234ede8a78e831889527469a439aabf042e2c
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-23 10:03:23 +0500

    tasks

--- 05-ustoz-darslari/lesson_23_meta_class.py
+++ 05-ustoz-darslari/lesson_23_meta_class.py
@@ -1,7 +1,28 @@
-A = type('Point', (), {'x':0, 'y':0})
+# # 1
+# A = type('Point', (), {'x':0, 'y':0})
 
-a = A()
-# a.x = 3
-print(a.x)
+# a = A()
+# # a.x = 3
+# print(a.x)
 
 
+# 2
+class Women:
+    women_id = "This is women id"
+    women_name = "This is women name"
+    women_age = "This is women age"
+
+    def __init__(self, login, pasw):
+        self.login = login
+        self.pasw = pasw
+        self.meta = self.Meta(login + "@" + pasw)
+    
+    class Meta:
+        def __init__(self, access):
+            self._access = access
+
+w = Women('root', '1234')
+print(w.__dict__)
+print(w.meta.__dict__)
+        
+        
