305028b228424b665b9db4716a7880a6b23847d7
Sobirjon005 <sobirjon0305@gmail.com>
2024-11-28 10:51:40 +0500

    tasks

--- 05-ustoz-darslari/lesson26_absract.py
+++ 05-ustoz-darslari/lesson26_absract.py
@@ -43,59 +43,80 @@
 #     pass
     
     
-        
+# # 2
+# from abc import ABC, abstractmethod
 
-
-
-from abc import ABC, abstractmethod
-
-class Figure(ABC):
-    def __init__(self, r=None, s1=None, s2=None):
-        self.radius = r
-        self.side1 = s1
-        self.side2 = s2
+# class Figure(ABC):
+#     def __init__(self, r=None, s1=None, s2=None):
+#         self.radius = r
+#         self.side1 = s1
+#         self.side2 = s2
             
-    @abstractmethod
-    def area(self):
-        pass
+#     @abstractmethod
+#     def area(self):
+#         pass
     
-    @abstractmethod
-    def perimetr(self):
-        pass
+#     @abstractmethod
+#     def perimetr(self):
+#         pass
     
-    @classmethod
-    def info(cls):
-        return cls.__name__
+#     @classmethod
+#     def info(cls):
+#         return cls.__name__
 
-class Rectangle(Figure):
-    def __init__(self, side1, side2):
-        super().__init__(s1=side1, s2=side2)
+# class Rectangle(Figure):
+#     def __init__(self, side1, side2):
+#         super().__init__(s1=side1, s2=side2)
     
-    def area(self):
-        return self.side1 * self.side2
+#     def area(self):
+#         return self.side1 * self.side2
     
-    def perimetr(self):
-        return 2 * (self.side1 + self.side2)
+#     def perimetr(self):
+#         return 2 * (self.side1 + self.side2)
 
-class Circle(Figure):
-    def __init__(self, radius):
-        super().__init__(r=radius)
-        self.pi = 3.14
+# class Circle(Figure):
+#     def __init__(self, radius):
+#         super().__init__(r=radius)
+#         self.pi = 3.14
 
-    def area(self):
-        return self.pi * self.radius ** 2
+#     def area(self):
+#         return self.pi * self.radius ** 2
     
-    def perimetr(self):
-        return 2 * self.pi * self.radius
+#     def perimetr(self):
+#         return 2 * self.pi * self.radius
 
-rectangle = Rectangle(4, 5)
-print(rectangle.area())
-print(rectangle.perimetr())
+# rectangle = Rectangle(4, 5)
+# print(rectangle.area())
+# print(rectangle.perimetr())
 
-print(rectangle.info())
+# print(rectangle.info())
 
-circle = Circle(3)
-print(circle.area())
-print(circle.perimetr())
+# circle = Circle(3)
+# print(circle.area())
+# print(circle.perimetr())
 
-print(circle.info())
+# print(circle.info())
+
+
+class Bank:
+    def __init__(self, balance: float):
+        self.balance = balance
+        
+    def add(self, new_balance):
+        self.balance += new_balance
+    
+    def get(self, payment):
+        self.balance -= payment
+    
+    def balance(self):
+        return f"your balance {self.balance}"
+        
+b1 = Bank(1000.1)
+
+print(b1.balance)
+
+b1.add(500.255)
+print(b1.balance)
+
+b1.get(200.91)
+print(b1.balance)
