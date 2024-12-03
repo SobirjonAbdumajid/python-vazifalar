b7e7241eba1bd951dea6bdd9fefa58b48406a581
Sobirjon005 <sobirjon0305@gmail.com>
2024-12-03 11:46:42 +0500

    tasks

--- 05-ustoz-darslari/lesson28_async.py
+++ 05-ustoz-darslari/lesson28_async.py
@@ -33,22 +33,52 @@
 #     asyncio.run(main(), debug=True)
 
 
-# 3
-import asyncio
-import random
+# # 3
+# import asyncio
+# import random
 
-async def prepare_order(name: str, preparing_time:int):
-    print(f"we started preparing {name} in {preparing_time}")
-    await asyncio.sleep(preparing_time)
-    print(f"we finished preparing {name} in {preparing_time}")
+# async def prepare_order(name: str, preparing_time:int):
+#     print(f"we started preparing {name} in {preparing_time}")
+#     await asyncio.sleep(preparing_time)
+#     print(f"we finished preparing {name} in {preparing_time}")
 
-async def main():
-    print("Started...")
-    orders = ["Burger", "lavash", "Hotdog"]
+# async def main():
+#     print("Started...")
+#     orders = ["Burger", "Lavash", "Hotdog", "Osh"]
     
-    order_list = [prepare_order(order, random.randint(1,10)) for order in orders]
-    await asyncio.gather(*order_list)
+#     order_list = [prepare_order(order, random.randint(1,10)) for order in orders]
+#     await asyncio.gather(*order_list)
+#     print("...Ended")
+
+# if __name__ == "__main__":
+#     asyncio.run(main(), debug=True)
+
+
+# 4
+import asyncio
+
+async def fetch_data(name:str, delay: int):
+    print(f"Fetiching {name} in {delay}")
+    await asyncio.sleep(delay)
+    print(f"Finished {name} in {delay}")
+
+async def process_data(data:dict) -> None:
+    print("Started...")
+    keys = list(data.keys())
+    values = list(data.values())
+    data_of = [fetch_data(keys[i], values[i]) for i in range(len(data))]
+    await asyncio.gather(*data_of)
     print("...Ended")
 
+
+apis = {
+    "Review Api": 2,
+    "User Api": 5,
+    "Product Api": 4,
+    "Owner Api": 3,
+    "Orders Api": 6
+}
+
 if __name__ == "__main__":
-    asyncio.run(main(), debug=True)
+    asyncio.run(process_data(apis), debug=True)
+    
