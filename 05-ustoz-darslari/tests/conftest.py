e34210bfb05e95cbfccc437aed2f6d0879af3c81
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-22 10:15:20 +0500

    faker

---
+++ 05-ustoz-darslari/tests/conftest.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def example_data():
+    return {"name": "Sobirjon", "age": 19}
+
+@pytest.fixture
+def temp_file():
+    file = Path("test.txt")
+    file.write_text("Hello PDP")
+    # return file
+    yield file
+    file.unlink(missing_ok=True)
