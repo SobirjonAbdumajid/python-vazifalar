7ff37eda4a71f45bdaa414a7a83e1dc5a240b29a
Sobirjon005 <sobirjon0305@gmail.com>
2025-02-26 16:50:53 +0500

    teachers task related with pytest is done

--- 05-ustoz-darslari/tests/test_fixture_data.py
+++ 05-ustoz-darslari/tests/test_fixture_data.py
@@ -13,12 +13,12 @@ def mock_user_data():
     print("\nSession scope fixture was closed")
 
 
-def test_user_data_1():
+def test_user_data_1(mock_user_data):
     pass
-    # assert mock_user_data["id"] == 1
-    # assert mock_user_data["username"] == "alice"
+    assert mock_user_data["id"] == 1
+    assert mock_user_data["username"] == "alice"
 
 
-# def test_user_data_2(mock_user_data):
-#     assert mock_user_data["email"] == "alica@gmail.com"
-#     assert mock_user_data["age"] == 15
+def test_user_data_2(mock_user_data):
+    assert mock_user_data["email"] == "alica@gmail.com"
+    assert mock_user_data["age"] == 15
