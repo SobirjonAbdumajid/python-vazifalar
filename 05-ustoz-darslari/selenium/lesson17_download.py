687b48595ea9a4ad45ac5e08333287015c51d0fc
Sobirjon005 <sobirjon0305@gmail.com>
2025-04-05 11:52:36 +0500

    selenium

---
+++ 05-ustoz-darslari/selenium/lesson17_download.py
@@ -0,0 +1,19 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+selenium_options = webdriver.ChromeOptions()
+
+default_download_path = {
+    'download.default_directory': f'{os.getcwd()}/downloads'
+}
+
+selenium_options.add_experimental_option('prefs', default_download_path)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=selenium_options)
+driver.get("https://shaxzodbek.com/")
+time.sleep(2)
+driver.find_elements('xpath', '//a')[10].click()
+time.sleep(2)
