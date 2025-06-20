dc3721c1c1d9311f705a397263ceff342fd645e1
Sobirjon <sobirjon0305@gmail.com>
2025-06-20 19:08:23 +0500

    Add and update lesson_03_csv.py

---
+++ 08-pandas/lesson_03_csv.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv('files/talabalar.csv')
+
+# print(df.head())           # Yuqoridan 5 ta
+# print(df.tail(2))          # Oxirgi 2 ta
+# print("Shape:", df.shape)  # Qator va ustun soni
+# print("Columns:", df.columns)
+# print(df.describe())       # Statistika
+
+# print(df)
